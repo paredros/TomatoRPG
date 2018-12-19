389866d01969fe231a803ce9f2df4dfa960ea9e3
paredros <paredros@gmail.com>
2018-12-19 02:36:49 -0300

    clock app ya con relax, aun le falta para terminarse

--- TomatoRPG/static/jsang/app.js
+++ TomatoRPG/static/jsang/app.js
@@ -23,16 +23,20 @@ app.controller('MainCtrl', function ($scope) {
 
 app.controller('ClockApp', function ($scope,$timeout) {
     var TIME_POMODORO=25;//in minutes
+    var TIME_RELAX_SHORT=5;
+    var TIME_RELAX_LONG=15;
+    $scope.ammount=0;
+    $scope.ammountMax=10;
     $scope.status = "stopped";
-    $scope.contador = 0;
+    $scope.momentum="pomodoro";
     $scope.timer = 0;
     var timerTo = new Date();
     var timerToDiffMax = new Date();
     var timerHold = 0;
+    var timerPreRelax=0;
     $scope.timerNorm=100;
     var updateClock = function () {
-        if($scope.status=="running"){
-            $timeout(updateClock, 1000);
+        if($scope.status=="running" || $scope.status=="relaxing"){
             var d = new Date();
             $scope.timer = timerTo-d;
             //console.log("ACA" + timerTo+":"+$scope.timer + " , " +(timerTo-d));
@@ -41,37 +45,89 @@ app.controller('ClockApp', function ($scope,$timeout) {
             if($scope.timer<=0){
                 stopClock();
             }
+        }else if($scope.status=="torelax"){
+            var d = new Date();
+            if(timerPreRelax-d <= 0){
+                stopClock();
+            }
         }
-
+        $timeout(updateClock, 1000);
     };
+    var resetClock = function (tt) {
+        var d = new Date();
+        d.setSeconds(d.getSeconds() + tt);
+        $scope.timer = d - (new Date());
+    };
+    var getFutureTime = function (tt) {
+        var d = getFutureTimePre(tt);
+        return d - (new Date());
+    }
+    var getFutureTimePre = function (tt) {
+        var d = new Date();
+        return d.setSeconds(d.getSeconds() + tt);
+    }
+    var getFutureTimePreMillis = function (tt) {
+        var d = new Date();
+        return d.setMilliseconds(d.getMilliseconds() + tt);
+    }
+
     var stopClock = function(){
-        $scope.status="stopped";
-        $scope.timer = 0;
-        $scope.timerNorm=100;
+        if($scope.status=="running") {
+            $scope.status = "torelax";
+            $scope.momentum = "relax";
+            $scope.ammount=$scope.ammount+1;
+            timerPreRelax = getFutureTimePre(TIME_RELAX_SHORT);
+            resetClock(TIME_RELAX_SHORT);
+        }else if($scope.status=="relaxing" || $scope.status=="torelax"){
+            $scope.status = "stopped";
+            $scope.momentum = "pomodoro";
+            resetClock(TIME_POMODORO);
+        }
+        $scope.timerNorm = 100;
     };
     $scope.pauseButton = function () {
         $scope.status="paused";
-
     };
     $scope.continueButton = function () {
         //timerTo = (new Date())+millis($scope.timer);
-        var d = new Date();
-        timerTo = d.setMilliseconds( d.getMilliseconds() + $scope.timer);
+        //var d = new Date();
+        //timerTo = d.setMilliseconds( d.getMilliseconds() + $scope.timer);
+        timerTo = getFutureTimePreMillis($scope.timer);
         //console.log(timerTo+":"+$scope.timer);
         $scope.status="running";
-        updateClock();
-    }
+        //updateClock();
+    };
+    $scope.startRelax = function(){
+        if($scope.status=="torelax"){
+            $scope.status="relaxing";
+            //var d = new Date();
+            //d.setMinutes(d.getMinutes() + TIME_POMODORO);
+            //d.setSeconds(d.getSeconds() + TIME_RELAX_SHORT);
+            //timerToDiffMax = d - (new Date());
+            timerToDiffMax = getFutureTime(TIME_RELAX_SHORT);
+            timerTo=getFutureTimePre(TIME_RELAX_SHORT);
+            $scope.timerNorm=100;
+            //updateClock();
+        };
+    };
+    $scope.stopRelax=function(){
+        stopClock();
+    };
     $scope.startButton = function () {
         if($scope.status=="stopped"){
             $scope.status="running";
-            var d = new Date();
-            d.setMinutes(d.getMinutes() + TIME_POMODORO);
-            timerToDiffMax = d - (new Date());
-            timerTo=d;
+            //var d = new Date();
+            //d.setMinutes(d.getMinutes() + TIME_POMODORO);
+            //d.setSeconds(d.getSeconds() + TIME_POMODORO);
+            //timerToDiffMax = d - (new Date());
+            timerToDiffMax = getFutureTime(TIME_POMODORO);
+            timerTo=getFutureTimePre(TIME_POMODORO);
             $scope.timerNorm=100;
-            updateClock();
+            //updateClock();
         };
     };
+    resetClock(TIME_POMODORO);
+    updateClock();
 });
 
 
